eead97e5c6371eac04dc3d55bee6c085767421fa
Prashant Maurya <prashantmaurya384@gmail.com>
2025-08-16 12:04:55 +0000

    Fix all bugs and glitch

--- src/app/movies/[id]/movie-details-client.tsx
+++ src/app/movies/[id]/movie-details-client.tsx
@@ -86,7 +86,7 @@ export function MovieDetailsClient({ movie }: MovieDetailsClientProps) {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+              src="https://www.youtube.com/embed/jDO7clVUess"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
